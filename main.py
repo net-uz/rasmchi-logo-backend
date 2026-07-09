0e6eed1dc00d4233ee9a015819ef390b0317839a
net-uz <akobir7894@gmail.com>
2026-07-09 12:22:44 +0500

    Update main.py (CORS fix, deployment files)

--- main.py
+++ main.py
@@ -14,15 +14,9 @@ from PIL import Image
 
 app = FastAPI(title="Rasmchi Logo Backend")
 
-origins = [
-    "https://net-uz.github.io",
-    "http://localhost",
-    "http://127.0.0.1",
-    "*",
-]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origin_regex=r".*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
